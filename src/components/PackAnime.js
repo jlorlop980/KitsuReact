04e709431597b399482227aa7101a71654a76b95
jlorlop980 <92323841+jlorlop980@users.noreply.github.com>
2023-01-17 23:52:20 +0100

    Update PackAnime.js

--- src/components/PackAnime.js
+++ src/components/PackAnime.js
@@ -1,6 +1,16 @@
 import React from "react";
 import "./../css/PackAnime.css";
 
+/**
+ * @function
+ * @name PackAnime
+ * @param {Object} props - Propiedades del componente.
+ * @param {string} props.sauce - La URL de la imagen a mostrar.
+ * @param {string} props.name - El nombre del anime o manga a mostrar.
+ * @returns {JSX} 
+ * @description  JSX con la imagen y el nombre del anime o manga.
+ */
+
 //este es un componente simple que usaremos tanto en el listado como en la página de búsqueda
 //unicamente muestra una imagen y nombre que se le pasan como props
 function PackAnime(props) {
